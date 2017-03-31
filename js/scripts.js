ff5f308243f3e6a454a15c4251af4d4e563ff8e3
Ben Metzger <benjpac@gmail.com>
2017-03-31 10:11:37 -0700

    change algorithm to have less reptition

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,36 @@
+// function pingPong(number) {
+//   var answer = [];
+//   if (number )
+//   for (var i = 1; i <= number; i++) {
+//     if ((i % 3 === 0) && (i % 5 === 0)) {
+//       answer.push("Ping Pong");
+//     } else if (i % 3 === 0) {
+//       answer.push("Ping")
+//     } else if (i % 5 === 0) {
+//       answer.push("Pong")
+//     } else
+//       answer.push(i)
+//   }
+//   return answer
+// }
+
 function pingPong(number) {
-  var answer = [];
+  var answers = [];
+  debugger;
   for (var i = 1; i <= number; i++) {
-    if ((i % 3 === 0) && (i % 5 === 0)) {
-      answer.push("Ping Pong");
-    } else if (i % 3 === 0) {
-      answer.push("Ping")
-    } else if (i % 5 === 0) {
-      answer.push("Pong")
-    } else
-      answer.push(i)
+    var string = "";
+    if (i % 3 === 0) {
+      string += "Ping";
+    }
+    if (i % 5 === 0) {
+      string += "Pong"
+    }
+    if (!string) {
+      string = i;
+    }
+    answers.push(string);
   }
-  return answer
+  return answers
 }
 
 $(document).ready(function() {
